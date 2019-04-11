64a0685a346e8849d8b663e1059eb0f1bf42d803
ozymandias <onuratasaritas@gmail.com>
2019-04-11 21:24:09 +0300

    Add dynamic templates and stylesheet
    Added dynamic template with handlebars and styling, upgraded from basic html to hbs views

--- src/server.js
+++ src/server.js
@@ -1,18 +1,61 @@
 const express = require('express')
 const path = require('path')
+//for dynamic pages with templating, this project uses handlebars.js
+const hbs = require('hbs')
 
 const app = express()
-
-//include public folder to default route
+app.set('view engine', 'hbs')
+//define paths for Express.js config
 const publicDirectoryPath = path.join(__dirname, '/public')
-console.log(publicDirectoryPath);
+const partialsPath = path.join(__dirname, '/views/partials')
 
-//default route of the server
+hbs.registerPartials(partialsPath)
+// Setup static dir to serve
 app.use(express.static(publicDirectoryPath)) 
 
+//main route
+app.get('', (req,res) => {
+    res.render('index', {
+        title: 'API Mainpage',
+        description: 'Some description',
+        name: 'Onur Ata Saritas'
+    })
+})
 
-app.get('/api', (req,res) => {
-    res.send('API');
+//about route
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About Us',
+        name: 'Onur Ata Saritas'
+    })
+})
+
+//help route
+app.get('/help', (req,res) => {
+    res.render('help', {
+        title: 'Help Page',
+        description: '',
+        name: 'Onur Ata Saritas'
+    })
+})
+
+//api route
+app.get('/docs', (req,res) => {
+    res.render('docs', {
+        title: 'Documentation',
+        description: '',
+        name: 'Onur Ata Saritas'
+    })
+})
+
+// error route
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'Sorry! We couldn\'t find that',
+        description: 'That page has been removed or no longer exists.',
+        name: 'Onur Ata Saritas'
+
+    })
 })
 
 //specifying the port for the server to listen
